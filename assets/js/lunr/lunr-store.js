81bdd6cf3762bbc0c8a636d59b1e9940b87030c6
LanKuDot <airlanser@gmail.com>
2021-11-24 15:43:25 +0000

    Update from https://github.com/LanKuDot/blog-source/commit/430d5b246f6b77d14783e21a00fb1418044a2be4

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Post: Test Post",
         "excerpt":"This is head 1  This is head 2  This is head 3      item 1   item 2   item 3            item a           #include &lt;stdio.h&gt; int main() {     return 0; }   ","categories": ["blog"],
         "tags": ["Test"],
-        "url": "/blog/test-post/",
+        "url": "/blog/2021-10-test-post/",
         "teaser": null
       }]
